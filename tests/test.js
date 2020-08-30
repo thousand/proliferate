63473056521068811788cc6f4efe3c370418ea8f
Bryan Levay <bryan@bryanlevay.com>
2020-08-30 14:50:24 -0700

    set node requirement, add cleanup to test

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,15 @@
 const fs = require( 'fs/promises' );
-const path = require('path');
+const path = require( 'path' );
 const Proliferation = require( '../index' );
+
+const OUTPUT_PATH = path.join( __dirname, 'data' );
 
 async function setup () {
   // create a proliferation
   const p = new Proliferation( {
     count: 15,
     fileName: 'file-%ID%.json',
-    filePath: path.join( __dirname, 'data' ),
+    filePath: OUTPUT_PATH,
     content: '{ "id": "%ID%" }',
   } );
 
@@ -16,10 +18,10 @@ async function setup () {
 }
 
 async function testFilePaths () {
-  let files = await fs.readdir(path.join( __dirname, 'data' ));
+  let files = await fs.readdir( OUTPUT_PATH );
 
   if ( files.length !== 15 ) {
-    throw new Error( `File count incorrect, expected 15 got ${files.length}` );
+    throw new Error( `File count incorrect, expected 15 got ${ files.length }` );
   }
 
   files.forEach( ( f, i ) => {
@@ -27,28 +29,33 @@ async function testFilePaths () {
     if ( f !== `file-${ id }.json` ) {
       throw new Error( 'File name incorrect' );
     }
-  } )
+  } );
 }
 
 async function testFileContents () {
-  let files = await fs.readdir(path.join( __dirname, 'data' ));
+  let files = await fs.readdir( OUTPUT_PATH );
 
   if ( files.length !== 15 ) {
     throw new Error( 'File count incorrect' );
   }
 
   files.forEach( ( f, i ) => {
-    let content = require(path.join( __dirname, 'data', f ));
-    if (content.id !== i.toString().padStart( 2, 0 )) {
+    let content = require( path.join( OUTPUT_PATH, f ) );
+    if ( content.id !== i.toString().padStart( 2, 0 ) ) {
       throw new Error( 'File content incorrect' );
     }
-  } )
+  } );
 }
 
-async function run() {
+async function cleanup () {
+  fs.rmdir( OUTPUT_PATH, { recursive: true } );
+}
+
+async function run () {
   await setup();
   await testFilePaths();
   await testFileContents();
+  await cleanup();
 }
 
 run();
